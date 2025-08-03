67ec9250194070265e5d534c58f709cf57ad8723
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-08-02 21:07:29 -0400

    feat: create index file for UI component exports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,15 @@
+// ==========================================
+// EXPORTACIONES DE COMPONENTES UI
+// ==========================================
+
+// Componentes básicos
+export { default as Button } from './Button';
+export type { ButtonProps } from './Button';
+
+// TODO: Agregar más componentes conforme se vayan creando
+// export { default as Input } from './Input';
+// export { default as Card } from './Card';
+// export { default as Modal } from './Modal';
+// export { default as Table } from './Table';
+// export { default as Badge } from './Badge';
+// export { default as LoadingSpinner } from './LoadingSpinner';
