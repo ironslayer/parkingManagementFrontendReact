c37adfb012dc8c553bda887c8fa85400bed46979
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-08-02 21:21:28 -0400

    feat: add Input, Card, and Badge exports to UI component index

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,10 +6,16 @@
 export { default as Button } from './Button';
 export type { ButtonProps } from './Button';
 
+export { default as Input } from './Input';
+export type { InputProps } from './Input';
+
+export { default as Card, CardHeader, CardContent, CardFooter } from './Card';
+export type { CardProps, CardHeaderProps, CardContentProps, CardFooterProps } from './Card';
+
+export { default as Badge, VehicleTypeBadge, SessionStatusBadge, PaymentStatusBadge } from './Badge';
+export type { BadgeProps } from './Badge';
+
 // TODO: Agregar más componentes conforme se vayan creando
-// export { default as Input } from './Input';
-// export { default as Card } from './Card';
 // export { default as Modal } from './Modal';
 // export { default as Table } from './Table';
-// export { default as Badge } from './Badge';
 // export { default as LoadingSpinner } from './LoadingSpinner';
