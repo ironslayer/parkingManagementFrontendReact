7fc35d69916957dfd5e74400c022963cc8016c8a
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-08-03 20:13:45 -0400

    feat: add Vehicle components and types exports

---
+++ src/components/vehicles/index.ts
@@ -0,0 +1,12 @@
+// ==========================================
+// EXPORTACIONES DE COMPONENTES DE VEHÍCULOS
+// ==========================================
+
+export { VehicleList } from './VehicleList';
+export { default as VehicleListDefault } from './VehicleList';
+
+export { VehicleForm } from './VehicleForm';
+export { default as VehicleFormDefault } from './VehicleForm';
+
+// Tipos
+export type { Vehicle, VehicleType, CreateVehicleRequest, UpdateVehicleRequest } from '../../types';
