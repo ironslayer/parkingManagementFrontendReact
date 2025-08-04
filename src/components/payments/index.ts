d6969215157b1094634b186a97479f4c2fa62950
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-08-03 21:00:27 -0400

    feat: add payments components exports to index file

---
+++ src/components/payments/index.ts
@@ -0,0 +1,10 @@
+// ==========================================
+// PAYMENTS COMPONENTS EXPORTS
+// ==========================================
+export { default as PaymentStats } from './PaymentStats'
+export { PaymentStats as PaymentStatsComponent } from './PaymentStats'
+
+// Future exports for other payment components:
+// export { default as PaymentList } from './PaymentList'
+// export { default as PaymentModal } from './PaymentModal'
+// export { default as RefundModal } from './RefundModal'
