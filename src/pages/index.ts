15dc527c0a65a838065140736cf8552b2d25e665
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-08-04 01:59:34 -0400

    feat: add index file for exporting page components and checkpoint

---
+++ src/pages/index.ts
@@ -0,0 +1,11 @@
+// ==========================================
+// PAGES EXPORTS
+// ==========================================
+export { default as DashboardPage } from './DashboardPage'
+export { default as VehiclesPage } from './VehiclesPage'
+export { default as SessionsPage } from './SessionsPage'
+
+// Future exports:
+// export { default as ReportsPage } from './ReportsPage'
+// export { default as SettingsPage } from './SettingsPage'
+// export { default as UsersPage } from './UsersPage'
