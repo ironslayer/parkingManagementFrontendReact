dd92d0a294441dbbd8feff9611a316c7aeddda94
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-08-03 20:42:32 -0400

    feat: add index file to export UI and feature components

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+// UI Components
+export * from './ui'
+
+// Feature Components
+export * from './vehicles'
+export * from './sessions'
+export * from './auth'
+export * from './business'
+export * from './layout'
