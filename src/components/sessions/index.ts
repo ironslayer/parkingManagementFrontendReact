ad3dde9e7596291ceaed07529246b489cf8d1723
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-08-03 20:42:48 -0400

    feat: create index file for sessions components with future implementation notes

---
+++ src/components/sessions/index.ts
@@ -0,0 +1,4 @@
+export { SessionList } from './SessionList'
+export { SessionForm } from './SessionForm'
+export { default as SessionListDefault } from './SessionList'
+export { default as SessionFormDefault } from './SessionForm'
