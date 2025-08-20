c60a7db3463c19f0ea3807d30f8825c089db71eb
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-08-20 06:49:20 -0400

    feat: create index file for exporting UserForm and UserList components

---
+++ src/components/users/index.ts
@@ -0,0 +1,6 @@
+// ==========================================
+// EXPORTACIONES DE COMPONENTES DE USUARIOS
+// ==========================================
+
+export { UserForm } from './UserForm';
+export { UserList } from './UserList';
